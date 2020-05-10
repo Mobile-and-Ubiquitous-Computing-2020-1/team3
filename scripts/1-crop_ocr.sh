83cd8dbb1d991f8497d1af779e2bee8999b7173b
juppytt <kimjuhi96@snu.ac.kr>
2020-05-10 18:12:43 +0900

    [Update] check if the image has nutrition table

--- scripts/1-crop_ocr.sh
+++ scripts/1-crop_ocr.sh
@@ -4,6 +4,8 @@ EXT_DIR=${PWD}/../off-nutrition-table-extractor/nutrition_extractor
 OCR_DIR=${PWD}/../OCR
 RES_DIR=${PWD}/../result
 
+NT_MSG="No table!!"
+
 pushd $EXT_DIR
 
 for img in ${IMG_DIR}/image_*
@@ -11,7 +13,13 @@ do
     bname=$(basename $img)
     img_name=${bname%.*}
     echo "processing $img_name"
-    python detection.py -i $img -c
+    res=$(python detection.py -i $img -c 2>/dev/null)
+    #echo $res
+    if [[ "$res" == *"$NT_MSG"* ]]; then
+        echo "no table"
+        continue
+    fi
+
     pushd $OCR_DIR
     python3 ocr.py --image ${EXT_DIR}/data/result/cropped.jpg
     popd
