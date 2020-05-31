2020b4698e4824d00e20392d7fcde85731753ffe
juppytt <kimjuhi96@snu.ac.kr>
2020-05-31 21:16:08 +0900

    nutriseeon Android app baseline

---
+++ nutriseeon/app/src/main/java/com/example/nutriseeon/NutriActivity.java
@@ -0,0 +1,14 @@
+package com.example.nutriseeon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NutriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutri);
+    }
+
+}
