7dd90fb57b55ecaf31a06bb9863e0d95b1cb61e9
juppytt <kimjuhi96@snu.ac.kr>
2020-06-03 00:29:43 +0900

    user-specific nutrition configuration

--- nutriseeon/app/src/main/java/com/example/nutriseeon/MainActivity.java
+++ nutriseeon/app/src/main/java/com/example/nutriseeon/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.nutriseeon;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int REQUEST_CODE = 1;
+    public int num = 5;
+    public boolean[] nutriSet = new boolean[num];
+    private boolean[] nutriSetFromD = new boolean[num];
+    private boolean[] nutriSetFromN = new boolean[num];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +27,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, REQUEST_CODE);
+                intent.putExtra("num", num);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.e("LOG", "main received");
+                boolean[] resSet = data.getBooleanArrayExtra("result_setting");
+                String result = "Target nutritions: \n";
+                if(resSet[0]) result += "\nCarbohydrate";
+                if(resSet[1]) result += "\nProtein";
+                if(resSet[2]) result += "\nFat";
+                if(resSet[3]) result += "\nSodium";
+                if(resSet[4]) result += "\nSugar";
+                Log.e("LOG", "main res: "+result);
+                Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- nutriseeon/app/src/main/java/com/example/nutriseeon/NutriActivity.java
+++ nutriseeon/app/src/main/java/com/example/nutriseeon/NutriActivity.java
@@ -1,7 +1,15 @@
 package com.example.nutriseeon;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class NutriActivity extends AppCompatActivity {
 
@@ -9,6 +17,62 @@ public class NutriActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutri);
-    }
 
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        final CheckBox cb1 = (CheckBox)findViewById(R.id.checkBox1);
+        final CheckBox cb2 = (CheckBox)findViewById(R.id.checkBox2);
+        final CheckBox cb3 = (CheckBox)findViewById(R.id.checkBox3);
+        final CheckBox cb4 = (CheckBox)findViewById(R.id.checkBox4);
+        final CheckBox cb5 = (CheckBox)findViewById(R.id.checkBox5);
+
+        cb1.setChecked(pref.getBoolean("check1", false));
+        cb2.setChecked(pref.getBoolean("check2", false));
+        cb3.setChecked(pref.getBoolean("check3", false));
+        cb4.setChecked(pref.getBoolean("check4", false));
+        cb5.setChecked(pref.getBoolean("check5", false));
+
+        Button b = (Button)findViewById(R.id.buttonSave2);
+        Log.e("LOG", "setNutri created");
+
+        b.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("LOG", "setNutri click");
+
+                Intent intent = new Intent();
+                if (cb1.isChecked())
+                    intent.putExtra("Carbohydrate", true);
+                if (cb2.isChecked())
+                    intent.putExtra("Protein", true);
+                if (cb3.isChecked())
+                    intent.putExtra("Fat", true);
+                if (cb4.isChecked())
+                    intent.putExtra("Sodium", true);
+                if (cb5.isChecked())
+                    intent.putExtra("Sugar", true);
+                setResult(RESULT_OK, intent);
+                finish();
+            } // end onClick
+        }); // end setOnClickListener
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        Log.e("LOG", "setNutri stop");
+
+        CheckBox cb1 = (CheckBox)findViewById(R.id.checkBox1);
+        CheckBox cb2 = (CheckBox)findViewById(R.id.checkBox2);
+        CheckBox cb3 = (CheckBox)findViewById(R.id.checkBox3);
+        CheckBox cb4 = (CheckBox)findViewById(R.id.checkBox4);
+        CheckBox cb5 = (CheckBox)findViewById(R.id.checkBox5);
+
+        editor.putBoolean("check1", cb1.isChecked());
+        editor.putBoolean("check2", cb2.isChecked());
+        editor.putBoolean("check3", cb3.isChecked());
+        editor.putBoolean("check4", cb4.isChecked());
+        editor.putBoolean("check5", cb5.isChecked());
+        editor.apply();
+    }
 }
