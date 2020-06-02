d5bcc9d8da08266f5e9838229e1d40673d549a91
juppytt <kimjuhi96@snu.ac.kr>
2020-06-03 00:29:43 +0900

    user-specific nutrition configuration

--- nutriseeon/app/src/main/java/com/example/nutriseeon/DiseaseActivity.java
+++ nutriseeon/app/src/main/java/com/example/nutriseeon/DiseaseActivity.java
@@ -1,13 +1,87 @@
 package com.example.nutriseeon;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 public class DiseaseActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disease);
+
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        final CheckBox cb1 = (CheckBox)findViewById(R.id.checkBox1);
+        final CheckBox cb2 = (CheckBox)findViewById(R.id.checkBox2);
+        final CheckBox cb3 = (CheckBox)findViewById(R.id.checkBox3);
+
+        cb1.setChecked(pref.getBoolean("check1", false));
+        cb2.setChecked(pref.getBoolean("check2", false));
+        cb3.setChecked(pref.getBoolean("check3", false));
+
+        Button b = (Button)findViewById(R.id.buttonSave);
+        final TextView tv = (TextView)findViewById(R.id.textView2);
+        Log.e("LOG", "setDisease created");
+
+        b.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("LOG", "setDisease click");
+                String result = "";
+                if(cb1.isChecked()) result += cb1.getText().toString() + "\n";
+                if(cb2.isChecked()) result += cb2.getText().toString() + "\n";
+                if(cb3.isChecked()) result += cb3.getText().toString() + "\n";
+                tv.setText("Results:\n" + result);
+
+                Intent intent = new Intent();
+                if (cb1.isChecked()) {
+                    intent.putExtra("Carbohydrate", true);
+                    intent.putExtra("Sugar", true);
+                    intent.putExtra("Fat", true);
+                }
+                if (cb2.isChecked()) {
+                    intent.putExtra("Carbohydrate", true);
+                    intent.putExtra("Sugar", true);
+                }
+                if (cb3.isChecked()) {
+                    intent.putExtra("Fat", true);
+                    intent.putExtra("Sodium", true);
+                }
+                setResult(RESULT_OK, intent);
+                finish();
+            } // end onClick
+        }); // end setOnClickListener
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        Log.e("LOG", "setDisease stop");
+
+        // Diabetes - Carbohydrate, Sugar, Fat
+        CheckBox cb1 = (CheckBox)findViewById(R.id.checkBox1);
+        // Obesity - Carbohydrate, Sugar
+        CheckBox cb2 = (CheckBox)findViewById(R.id.checkBox2);
+        // High pressure -  Fat, Sodium
+        CheckBox cb3 = (CheckBox)findViewById(R.id.checkBox3);
+
+        editor.putBoolean("check1", cb1.isChecked());
+        editor.putBoolean("check2", cb2.isChecked());
+        editor.putBoolean("check3", cb3.isChecked());
+        editor.apply();
     }
 }
--- nutriseeon/app/src/main/java/com/example/nutriseeon/SettingActivity.java
+++ nutriseeon/app/src/main/java/com/example/nutriseeon/SettingActivity.java
@@ -1,23 +1,39 @@
 package com.example.nutriseeon;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SettingActivity extends AppCompatActivity {
+    private int REQUEST_DISEASE = 2;
+    private int REQUEST_NUTRI = 3;
+
+    private int num = 5;
+    private boolean[] nutriSet = new boolean[num];
+    private boolean[] nutriSetFromD = new boolean[num];
+    private boolean[] nutriSetFromN = new boolean[num];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
         Button button1 = (Button) findViewById(R.id.diseaseManageButton);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), DiseaseActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, REQUEST_DISEASE);
             }
         });
         Button button2 = (Button) findViewById(R.id.nutriManageButton);
@@ -25,8 +41,94 @@ public class SettingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), NutriActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, REQUEST_NUTRI);
             }
         });
+        Button button3 = (Button)findViewById(R.id.buttonSave3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnToMain();
+            }
+        });
+
     }
+
+    private void mergeResults() {
+        for (int i=0; i<num; i++) {
+            nutriSet[i] = nutriSetFromD[i] || nutriSetFromN[i];
+        }
+    }
+    private void updateFromResult(boolean[] resArr, Intent data) {
+        Arrays.fill(resArr, false);
+        if (data.getBooleanExtra("Carbohydrate", false))
+            resArr[0] = true;
+        if (data.getBooleanExtra("Protein", false))
+            resArr[1] = true;
+        if (data.getBooleanExtra("Fat", false))
+            resArr[2] = true;
+        if (data.getBooleanExtra("Sodium", false))
+            resArr[3] = true;
+        if (data.getBooleanExtra("Sugar", false))
+            resArr[4] = true;
+        mergeResults();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_DISEASE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.e("LOG", "setDisease received");
+                updateFromResult(nutriSetFromD, data);
+                printNutri();
+                //returnToMain();
+            }
+        } else if (requestCode == REQUEST_NUTRI) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.e("LOG", "setNutri received");
+                updateFromResult(nutriSetFromN, data);
+                printNutri();
+                //returnToMain();
+            }
+        }
+    }
+
+    void printNutri() {
+        String result = "Target nutritions: \n";
+        if(nutriSet[0]) result += "\nCarbohydrate";
+        if(nutriSet[1]) result += "\nProtein";
+        if(nutriSet[2]) result += "\nFat";
+        if(nutriSet[3]) result += "\nSodium";
+        if(nutriSet[4]) result += "\nSugar";
+        Log.e("LOG", "main res: "+result);
+        Toast.makeText(SettingActivity.this, result, Toast.LENGTH_LONG).show();
+    }
+    void returnToMain() {
+        Log.e("LOG", "setting return");
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("nutri0", nutriSet[0]);
+        editor.putBoolean("nutri1", nutriSet[1]);
+        editor.putBoolean("nutri2", nutriSet[2]);
+        editor.putBoolean("nutri3", nutriSet[3]);
+        editor.putBoolean("nutri4", nutriSet[4]);
+        editor.apply();
+        Intent intentSetting = new Intent();
+
+        String result = "";
+        if(nutriSet[0]) result += "Carbohydrate\n";
+        if(nutriSet[1]) result += "Protein\n";
+        if(nutriSet[2]) result += "Fat\n";
+        if(nutriSet[3]) result += "Sodium\n";
+        if(nutriSet[4]) result += "Sugar\n";
+        Log.e("LOG", "setting res: "+result);
+
+        intentSetting.putExtra("result_msg", "here it goes");
+        intentSetting.putExtra("result_setting", nutriSet);
+        setResult(RESULT_OK, intentSetting);
+        finish();
+    }
+
+
 }
