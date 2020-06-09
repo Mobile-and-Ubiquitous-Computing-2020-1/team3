5477dd67f3203c058e7e0848eaf2d3d67492fbb5
kev-damian <maheu@outlook.com>
2020-06-09 21:33:20 +0900

    request permissions

--- nutriseeon/app/src/main/java/com/example/nutriseeon/MainActivity.java
+++ nutriseeon/app/src/main/java/com/example/nutriseeon/MainActivity.java
@@ -2,13 +2,12 @@ package com.example.nutriseeon;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -19,8 +18,17 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private int SETTING_REQUEST_CODE = 1;
     private int DEVICE_REQUEST_CODE = 10;
-    private int PERMISSION_REQUEST_CAMERA = 1001;
-    private int PERMISSION_REQUEST_BLUETOOTH = 1002;
+
+
+    int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.CAMERA
+    };
+
 
     //public boolean[] nutriSet = new boolean[Nutritions.values().length];
     public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
@@ -72,7 +80,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setPermission();
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
         Button BleButton = (Button) findViewById(R.id.bleButton);
         BleButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -135,21 +145,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    void setPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA},
-                    PERMISSION_REQUEST_CAMERA);
-            Toast.makeText(this,"Need permissions",Toast.LENGTH_LONG).show();
-        }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN},
-                    PERMISSION_REQUEST_BLUETOOTH);
 
-            Toast.makeText(this,"Need permissions",Toast.LENGTH_LONG).show();
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
         }
+        return true;
     }
+
 }
