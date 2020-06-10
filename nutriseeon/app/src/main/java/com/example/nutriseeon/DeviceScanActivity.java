868243acac4d06220dd755e41d3621379919f8b2
kev-damian <maheu@outlook.com>
2020-06-10 23:48:25 +0900

    edit DeviceScanActivity not to return from result activity

--- nutriseeon/app/src/main/java/com/example/nutriseeon/DeviceScanActivity.java
+++ nutriseeon/app/src/main/java/com/example/nutriseeon/DeviceScanActivity.java
@@ -148,10 +148,10 @@ public class DeviceScanActivity extends ListActivity {
 
         Log.e("LOG", "Device Clicked!");
 
-        final Intent intent = new Intent(this, CameraActivity.class);
+        final Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
         intent.putExtra(CameraActivity.EXTRAS_DEVICE_NAME, device.getName());
         intent.putExtra(CameraActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         if (mScanning) {
             mBLEScanner.stopScan(mScanCallback);
